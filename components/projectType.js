2cd8581a4b728545891d76725977f7716428c727
Naman Dhingra <namandhingra90@gmail.com>
2025-07-20 14:09:34 +0530

    refactor: Clean up code formatting and improve readability in projectType component

--- components/projectType.js
+++ components/projectType.js
@@ -1,8 +1,8 @@
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 import { Fragment, useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import style from './projectType.module.css'
-import Aos from 'aos'
+import style from "./projectType.module.css";
+import Aos from "aos";
 
 export const Projecttype = (props) => {
   const [openproject, setProject] = useState(false);
@@ -11,9 +11,9 @@ export const Projecttype = (props) => {
       return !prev;
     });
   };
-  useEffect(()=>{
+  useEffect(() => {
     Aos.refresh();
-  },[openproject])
+  }, [openproject]);
   return (
     <div className="row">
       <div
@@ -28,36 +28,36 @@ export const Projecttype = (props) => {
         className="bg-primary w-100 col-12 me-3"
         style={{ backgroundColor: "red", height: "1px" }}
       ></div>
-      <div className={`col-12 ${
-                openproject ? `d-none` : ''
-              } `+style.animate}>
+      <div className={`col-12 ${openproject ? `d-none` : ""} ` + style.animate}>
         {props.projects.map((x, i) => {
           return (
             <div
               className={`row   border rounded-top rounded-bottom my-3 mx-1 py-2`}
               key={i}
               data-aos="fade-right"
-                data-aos-offset="100"
-                data-aos-delay="200"
-                data-aos-duration="1500"
+              data-aos-offset="100"
+              data-aos-delay="200"
+              data-aos-duration="1500"
             >
               <div className="col-lg-3 col-md-12 justify-content-around d-flex flex-column py-4">
                 <img src={`${x.imageLink}`} className={`w-100`}></img>
                 <div>
-                  <div className="me-3">
+                  {/* <div className="me-3">
                     <a className="me-1" href={`${x.projectlink}`}><u>Project link</u></a>
                     <FontAwesomeIcon
                       icon={faArrowUpRightFromSquare}
                       size="xs"
                     />
+                  </div> */}
+                  <div className="me-2">
+                    <a className="me-1" href={`${x.githublink}`}>
+                      <u>Github</u>
+                    </a>
+                    <FontAwesomeIcon
+                      icon={faArrowUpRightFromSquare}
+                      size="xs"
+                    />
                   </div>
-                    <div className="me-2">
-                      <a className="me-1" href={`${x.githublink}`}><u>Github</u></a>
-                      <FontAwesomeIcon
-                        icon={faArrowUpRightFromSquare}
-                        size="xs"
-                      />
-                    </div>
                 </div>
               </div>
               <div className={`col-lg-9 col-md-12`}>
@@ -74,7 +74,6 @@ export const Projecttype = (props) => {
                     </p>
                   );
                 })}
-                
               </div>
             </div>
           );
